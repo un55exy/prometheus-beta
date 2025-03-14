89456fced81b95e08279740a84e0605f7d1eb6e0
un55exy <un55exy@users.noreply.github.com>
2025-03-14 02:51:13 +0000

    Final revision of modified Fibonacci implementation

--- src/modified_fibonacci.py
+++ src/modified_fibonacci.py
@@ -27,7 +27,7 @@ def generate_modified_fibonacci(n):
 
     # Generate sequence while modifying to meet divisibility condition
     while len(sequence) < n:
-        # Adjust the next number to make the sum of previous two divisible by 3
+        # Compute next number to ensure divisibility from 3rd element
         next_num = 3 - (sequence[-1] + sequence[-2]) % 3 + sequence[-1] + sequence[-2]
         sequence.append(next_num)
 

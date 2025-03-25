62cd6b2bca363c6419b5c9b8a65e3f99e4f290e2
un55exy <un55exy@users.noreply.github.com>
2025-03-25 01:41:08 +0000

    Fix readable permissions format

--- src/file_permissions.py
+++ src/file_permissions.py
@@ -43,12 +43,16 @@ def get_file_permissions(file_path):
         
         # Create readable permission string
         readable_permissions = ''
-        for who in ['USR', 'GRP', 'OTH']:
-            for perm in ['R', 'W', 'X']:
-                symbol = '-'
-                if getattr(stat, f'S_I{perm}{who}'):
-                    symbol = 'r' if perm == 'R' else 'w' if perm == 'W' else 'x'
-                readable_permissions += symbol
+        permission_groups = [
+            (stat.S_IRUSR, stat.S_IWUSR, stat.S_IXUSR),  # Owner
+            (stat.S_IRGRP, stat.S_IWGRP, stat.S_IXGRP),  # Group
+            (stat.S_IROTH, stat.S_IWOTH, stat.S_IXOTH)   # Others
+        ]
+        
+        for read_perm, write_perm, exec_perm in permission_groups:
+            readable_permissions += 'r' if mode & read_perm else '-'
+            readable_permissions += 'w' if mode & write_perm else '-'
+            readable_permissions += 'x' if mode & exec_perm else '-'
         
         return {
             'numeric': numeric_permissions,
